3e707dae0df88127f5b468a8ad0fd00bd0b7b2a7
xor0139 <xor0139@gmail.com>
2023-11-26 17:54:38 +0300

    add match answers

--- Core/handlers/exam.py
+++ Core/handlers/exam.py
@@ -3,7 +3,8 @@ from aiogram.fsm.context import FSMContext
 from aiogram.types import CallbackQuery
 from Core.keyboards.inline import get_inline_keyboard
 from Core.utils.steps_exam import StepsExam
-from Core.Dictonary import QuestionsA
+from Core.Dictonary import QuestionsA, AnswersA
+from Core.utils.match_answer import match
 
 async def start_exam(message: Message, state: FSMContext):
     await message.answer(f"{message.from_user.first_name} {QuestionsA.INTRO}")
@@ -22,8 +23,8 @@ async def finish(message: Message, state: FSMContext):
     await message.answer(f"{message.from_user.first_name} FINISH")
     await state.update_data(answer2=message.text)
     answers = await state.get_data()
-    txt = f"""Ваши ответы:\n
-              Ответ 1: {answers.get('answer1')}\n
-              Ответ 2: {answers.get('answer2')}\n"""
+    txt = f"Ваши результат:\n" \
+            f"Вопрос #1: {await match(str(answers.get('answer1')), AnswersA.ANSWER1)}\n" \
+            f"Вопрос #2: {await match(str(answers.get('answer2')), AnswersA.ANSWER2)}\n"
     await message.answer(txt)
     await state.clear()
